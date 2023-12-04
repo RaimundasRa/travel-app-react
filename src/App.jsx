71de780b917c9b46233f0e0eed129dd3244ea954
Raimundas Radavicius <raimundas_radavicius@yahoo.com>
2023-12-04 09:18:00 +0000

    login & logout

--- src/App.jsx
+++ src/App.jsx
@@ -11,32 +11,43 @@ import CountryList from "./components/CountryList.jsx";
 import City from "./components/City.jsx";
 import Form from "./components/Form.jsx";
 import { CitiesProvider } from "./contexts/CitiesContext.jsx";
+import { AuthProvider } from "./contexts/FakeAuthContext.jsx";
+import ProtectedRoute from "./pages/ProtectedRoute.jsx";
 
 function App() {
     return (
-        <CitiesProvider>
-            <BrowserRouter>
-                <Routes>
-                    <Route index element={<Homepage />} />
-                    <Route path="product" element={<Product />} />
-                    <Route path="pricing" element={<Pricing />} />
-                    <Route path="login" element={<Login />} />
-                    <Route path="app" element={<AppLayout />}>
-                        {/* in nested routes index creates default route if no further route specified */}
+        <AuthProvider>
+            <CitiesProvider>
+                <BrowserRouter>
+                    <Routes>
+                        <Route index element={<Homepage />} />
+                        <Route path="product" element={<Product />} />
+                        <Route path="pricing" element={<Pricing />} />
+                        <Route path="login" element={<Login />} />
                         <Route
-                            index
-                            element={<Navigate replace to="cities" />}
-                        />
-                        <Route path="cities" element={<CityList />} />
-                        <Route path="cities/:id" element={<City />} />
-                        <Route path="countries" element={<CountryList />} />
-                        <Route path="form" element={<Form />} />
-                    </Route>
-                    {/* "*" catches if there is no matching route */}
-                    <Route path="*" element={<PageNotFound />} />
-                </Routes>
-            </BrowserRouter>
-        </CitiesProvider>
+                            path="app"
+                            element={
+                                <ProtectedRoute>
+                                    <AppLayout />
+                                </ProtectedRoute>
+                            }
+                        >
+                            {/* in nested routes index creates default route if no further route specified */}
+                            <Route
+                                index
+                                element={<Navigate replace to="cities" />}
+                            />
+                            <Route path="cities" element={<CityList />} />
+                            <Route path="cities/:id" element={<City />} />
+                            <Route path="countries" element={<CountryList />} />
+                            <Route path="form" element={<Form />} />
+                        </Route>
+                        {/* "*" catches if there is no matching route */}
+                        <Route path="*" element={<PageNotFound />} />
+                    </Routes>
+                </BrowserRouter>
+            </CitiesProvider>
+        </AuthProvider>
     );
 }
 
